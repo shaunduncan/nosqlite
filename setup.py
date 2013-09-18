ba9e097f3d89c33672d084b27723d424dac35936
Shaun Duncan <shaun.duncan@gmail.com>
2013-09-18 17:54:44 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+version = '0.0.1'
+
+setup(name="nosqlite",
+      version=version,
+      description='A wrapper for sqlite3 to have schemaless, document-store features',
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+      keywords='nosql,sqlite,nosqlite',
+      author='Shaun Duncan',
+      author_email='shaun.duncan@gmail.com',
+      url='https://github.com/shaunduncan/nosqlite',
+      license='MIT',
+      py_modules=['nosqlite'],
+      include_package_data=True,
+)
